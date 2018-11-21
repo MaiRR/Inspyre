12656413073fc519c8076b79a6e190b685889eaa
dkeriazisStuy <dkeriazis@stuy.edu>
2018-11-21 00:17:48 -0500

    Added goals to users

--- util/users.py
+++ util/users.py
@@ -6,7 +6,9 @@ def create_db():
         CREATE TABLE users (
             username TEXT PRIMARY KEY,
             pass_hash BLOB NOT NULL,
-            salt BLOB NOT NULL
+            salt BLOB NOT NULL,
+            goal TEXT,
+            last_goal_time INT
         )
     ''')
     util.config.end_db(db)
