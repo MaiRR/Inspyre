636678fecc1d91bfd8622cbd196525d7de73f23a
dkeriazisStuy <dkeriazis@stuy.edu>
2018-11-20 21:18:59 -0500

    Added Bing Image of the Day API

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-# Team RedSky
-# SoftDev pd7
-
 from flask import Flask, render_template
+import util.apis
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        background=util.apis.image_of_the_day(),
+    )
 
 if __name__ == '__main__':
     app.debug = True
--- util/apis.py
+++ util/apis.py
@@ -1,3 +1,11 @@
+import urllib
+import json
+
 def image_of_the_day():
-    url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
-    prefix = 'https://www.bing.com'
+    url = \
+        'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
+    prefix = 'https://www.bing.com/'
+    with urllib.request.urlopen(url) as f:
+        result = json.loads(f.read())
+    return prefix + result['images'][0]['url']
+
