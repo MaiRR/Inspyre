e383fa28bc6be91f6a5095e46decde4b468c8530
dkeriazisStuy <dkeriazis@stuy.edu>
2018-11-26 21:24:37 -0500

    Added Oxford API usage

--- util/apis.py
+++ util/apis.py
@@ -1,6 +1,11 @@
 import urllib
+import urllib.request
+import urllib.parse
 import json
 import random
+import time
+
+import util.config
 
 def image_of_the_day():
     num_images = 8
@@ -13,4 +18,53 @@ def image_of_the_day():
     rand_image = random.choice(images)
     image_path = rand_image['url']
     return prefix + image_path
+
+def word_pair_of_the_day():
+    """
+    Returns a random word pair (a tuple of id, word)
+    seeded by the day in UTC time
+    """
+    app_id = util.config.get_oxford_api_id()
+    app_key = util.config.get_oxford_api_keys()
+    source_lang = 'en'
+    filters_raw = \
+        'lexicalCategory=noun,adjective,adverb,verb;' \
+        'registers=rare'
+    filters_basic = urllib.parse.quote(filters_raw)
+    url = \
+        'https://od-api.oxforddictionaries.com/api/v1/wordlist/' \
+        f'{source_lang}/{filters_basic}'
+    headers = {'app_id': app_id, 'app_key': app_key}
+    r = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(r) as f:
+        result = json.load(f)
+    words = result['results']
+    sec_in_day = 60 * 60 * 24  # 60 sec/min * 60 min/hr * 24 hr/day
+    day_num = int(time.time() / sec_in_day)
+    random.seed(day_num)  # Seed the word based on the current day
+    word_dict = random.choice(words)
+    return word_dict['id'], word_dict['word']
+
+def definition_of_the_day():
+    """
+    Returns the pair (word, definition) for a given day
+    """
+    app_id = util.config.get_oxford_api_id()
+    app_key = util.config.get_oxford_api_keys()
+    word_id, word = word_pair_of_the_day()
+    source_lang = 'en'
+    url = \
+        'https://od-api.oxforddictionaries.com/api/v1/entries/' \
+        f'{source_lang}/{word_id}'
+    headers = {'app_id': app_id, 'app_key': app_key}
+    r = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(r) as f:
+        result = json.load(f)
+    definition = result \
+        ['results'][0] \
+        ['lexicalEntries'][0] \
+        ['entries'][0] \
+        ['senses'][0] \
+        ['definitions'][0]
+    return word, definition
 
--- util/config.py
+++ util/config.py
@@ -1,10 +1,17 @@
 import os.path  # Used for file locations
 import sqlite3
+import json
+import random
+import time
 
 CUR_DIR = os.path.dirname(__file__)  # Absolute path to current directory
 ROOT_DIR = os.path.join(CUR_DIR, os.path.pardir)  # Location of root directory
 DATA_DIR = os.path.join(ROOT_DIR, 'data')  # Location of data directory
 DB_FILE = os.path.join(DATA_DIR, 'data.db')  # Location of database file
+APIS_FILE = os.path.join(DATA_DIR, 'apis.json')  # Location of apis config file
+
+# Set seed based on time in case it is reset
+random.seed(time.time())
 
 
 def use_test_db():
@@ -21,4 +28,16 @@ def start_db():
 def end_db(db):
     db.commit()  # Save changes to database
     db.close()  # Close database
+
+
+def get_oxford_api_id():
+    with open(APIS_FILE) as f:
+        result = json.load(f)
+    return result['oxford_api']['id']
+
+
+def get_oxford_api_keys():
+    with open(APIS_FILE) as f:
+        result = json.load(f)
+    return random.choice(result['oxford_api']['keys'])
 
