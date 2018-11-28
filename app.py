09dc7c1187e4b6c80981faf518e1f4a6c8410de4
dkeriazisStuy <dkeriazis@stuy.edu>
2018-11-27 21:19:43 -0500

    Renamed html template files

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
+
 import util.apis
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    word, definition = util.apis.definition_of_the_day()
     return render_template(
         'index.html',
         background=util.apis.image_of_the_day(),
+        definition=definition,
+        word=word,
     )
 
 if __name__ == '__main__':
