96368a5bacc2492a92ef574cc3dce110facc156f
dkeriazis <dkeriazis@gmail.com>
2018-11-28 12:18:29 -0500

    Added routes

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ def index():
         word=word,
     )
 
+@app.route('/login')
+def login():
+    return render_template(
+        'login.html',
+    )
+
+@app.route('/register')
+def register():
+    return render_template(
+        'register.html',
+    )
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
